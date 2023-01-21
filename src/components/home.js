39baf1a84cf9540e5801929ae8ff8f1684598822
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-01-21 19:58:17 +0300

    1

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+export function Home() {
+
+  return (
+    <div>
+      Hello!
+    </div>
+  );
+}
