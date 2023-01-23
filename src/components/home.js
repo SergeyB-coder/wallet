dc938445f09c1918b0885d1c36c01a3f547b84f9
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-01-23 23:41:54 +0300

    9

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
 // window.Telegram.WebApp
+const tg = window.Telegram.WebApp
 
 export function Home() {
 
   return (
     <div>
-      Hello!
+      <p>Hello!</p>
+      <input placeholder='Адрес'/>
     </div>
   );
 }
