623cb874935bad31536674862ba88e07ca0c384a
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-01-26 00:23:04 +0300

    42

--- src/components/Ptp/ptp.js
+++ src/components/Ptp/ptp.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { TradeMenu } from './trademenu';
 // import { useSelector, useDispatch } from 'react-redux';
 // window.Telegram.WebApp
 
 export function Ptp(props) {
   return (
-    <div>
+    <div className='p-4'>
       <h3>P2P</h3>
-      
+      <TradeMenu/>
     </div>
   );
 }
