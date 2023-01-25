bc047c14c919dacfa4fabec8d8fb227523a8dbe6
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-01-25 22:23:14 +0300

    36

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 // import logo from './logo.svg';
 import {
   BrowserRouter,
@@ -8,44 +8,40 @@ import {
 import './App.css';
 import { Home } from './components/Home/home';
 import { Ptp } from './components/Ptp/ptp';
-import { url } from './const/urls';
 import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
   const {tg} = useTelegram()
-  const [title, setTitle] = useState(0)
-  const [adress, setAdress] = useState('')
   
   
 
-  const onSendData = useCallback( () => {
-    console.log('adress', adress)
-    // tg.sendData(JSON.stringify({message: 'hello!'}))
-    fetch(url + '/checkadress', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        //   mode: 'no-cors',
-        body: JSON.stringify({
-          adress: adress
-        })
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('data', data)
-      setTitle(data.res)
-    });
-  }, [adress])
+//   const onSendData = useCallback( () => {
+//     // tg.sendData(JSON.stringify({message: 'hello!'}))
+//     fetch(url + '/checkadress', {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         //   mode: 'no-cors',
+//         body: JSON.stringify({
+//           adress: adress
+//         })
+//     })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log('data', data)
+//       setTitle(data.res)
+//     });
+//   }, [adress])
 
   useEffect(()=>{tg.ready()})
   useEffect(() => {tg.MainButton.show()})
-  useEffect(() => {
-    tg.onEvent('mainButtonClicked', onSendData)
-    return () => {tg.offEvent('mainButtonClicked', onSendData)}
-  }, [onSendData, tg])
+//   useEffect(() => {
+//     tg.onEvent('mainButtonClicked', onSendData)
+//     return () => {tg.offEvent('mainButtonClicked', onSendData)}
+//   }, [onSendData, tg])
   
   return (
     <div className="App">
