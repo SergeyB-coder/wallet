b4a43747871092089189dc0e026115ccefb874f2
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-02-10 21:04:03 +0300

    50

--- src/components/Home/button.js
+++ src/components/Home/button.js
@@ -6,7 +6,7 @@ export function Button (props) {
                 <div>
                     <div className="menu-button-svg-container-p">
                         <div className='menu-button-svg-container'>
-                            <svg stroke="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill={props.fill_svg} className="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <svg stroke="red" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill={props.fill_svg} className="bi bi-plus-lg" viewBox="0 0 16 16">
                                 <path  fillRule="evenodd" d={props.d_svg}/>
                             </svg>
                         </div>
