e76784266ccef875f779f9e29bb707a3e71bf6c6
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-02-14 20:26:20 +0300

    50

--- src/components/Home/menubuttons.js
+++ src/components/Home/menubuttons.js
@@ -91,7 +91,7 @@ export function MenuButtons() {
                         d_svg={'M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z'}
                         fill_svg={"var(--btn-bg-color)"}
                         onClick={handleClickSend}
-                        style={{ marginRight: 20}}
+                        // style={{ marginRight: 20}}
                     />
                 </div>
             </div>
