812826e5060b8293f65e4741ffbaf5d88f49d57e
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-02-14 20:15:56 +0300

    50

--- src/components/Home/button.js
+++ src/components/Home/button.js
@@ -6,7 +6,7 @@ export function Button (props) {
                 <div>
                     <div className="menu-button-svg-container-p">
                         <div className='menu-button-svg-container'>
-                            <svg stroke="white" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <svg stroke="white" xmlns="http://www.w3.org/2000/svg" width="18" height="18"  fill="" className="bi bi-plus-lg" viewBox="0 0 16 16">
                                 <path  fillRule="evenodd" d={props.d_svg}/>
                             </svg>
                         </div>
--- src/components/Home/menubuttons.js
+++ src/components/Home/menubuttons.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { svg_ptp_ico, svg_receive, svg_send } from '../../const/svgs';
 import { useTelegram } from '../../hooks/useTelegram';
+import { Button } from './button';
 // import { Button } from './button';
 import { getWallet, getWalletTRX } from './homeApi';
 import { selectAddress, selectAddressTRX, setAddress, setAddressTRX } from './homeSlice';
@@ -49,15 +50,15 @@ export function MenuButtons() {
         <>
             <div className='d-flex justify-content-center'>
                 <div className='row container-buttons m-0 p-0'>
-                    {/* <Button 
+                    <Button 
                         text={'Получить'} 
                         d_svg={'M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z'}
                         fill_svg={"var(--btn-bg-color)"}
                         onClick={handleClickAddress}
-                        style={{ marginLeft: 20}}
-                    /> */}
+                        // style={{ marginLeft: 20}}
+                    />
 
-                    <div className='button-svg'>
+                    {/* <div className='button-svg'>
                         <div  onClick={handleClickAddress}>
                             {svg_send}
                             <div style={{color: 'white', marginTop: 5}}>Получить</div>
@@ -76,10 +77,10 @@ export function MenuButtons() {
                             {svg_receive}
                             <div style={{color: 'white', marginTop: 5}}>Отправить</div>
                         </div>
-                    </div>
+                    </div> */}
                     
                         
-                    {/* <Button 
+                    <Button 
                         text={'P2P Маркет'} 
                         d_svg={'M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z'}
                         fill_svg={"var(--btn-bg-color)"}
@@ -91,7 +92,7 @@ export function MenuButtons() {
                         fill_svg={"var(--btn-bg-color)"}
                         onClick={handleClickSend}
                         style={{ marginRight: 20}}
-                    /> */}
+                    />
                 </div>
             </div>
         </>
