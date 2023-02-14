acc654fe7d5d5eac684a4fe8f183edcccc1b0305
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-02-14 16:06:05 +0300

    50

--- src/components/Home/menubuttons.js
+++ src/components/Home/menubuttons.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { svg_ptp, svg_ptp_ico, svg_receive, svg_send } from '../../const/svgs';
+import { svg_ptp_ico, svg_receive, svg_send } from '../../const/svgs';
 import { useTelegram } from '../../hooks/useTelegram';
-import { Button } from './button';
+// import { Button } from './button';
 import { getWallet, getWalletTRX } from './homeApi';
 import { selectAddress, selectAddressTRX, setAddress, setAddressTRX } from './homeSlice';
 import './style.css'
