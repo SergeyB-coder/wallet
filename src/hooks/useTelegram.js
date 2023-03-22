a8afe015a15b1c6dd454061ac80a0c7df402a644
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-03-23 02:13:30 +0300

    50

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -4,10 +4,10 @@ export function useTelegram () {
     return {
         tg,
         query_id: tg.initDataUnsafe.query_id,
-        user_id: tg.initDataUnsafe.user.id,
-        first_name: tg.initDataUnsafe.user.first_name,
-        // user_id: 638484379,
-        // first_name: 'Sash'
+        // user_id: tg.initDataUnsafe.user.id,
+        // first_name: tg.initDataUnsafe.user.first_name,
+        user_id: 638484379,
+        first_name: 'Sash'
         // user_id: '2125428302',
         // user_id: 652065848,
         // first_name: 'Srg',
@@ -15,6 +15,7 @@ export function useTelegram () {
         // first_name: 'Super'
     }
 }
+
 
 // const user_id = '652065848'
 // const user_id = '222'
