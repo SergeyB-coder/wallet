08b02331c781e3991de6826861f5d975536f708b
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-04-04 01:25:41 +0300

    50

--- src/components/Ptp/ptp.js
+++ src/components/Ptp/ptp.js
@@ -29,9 +29,9 @@ export function Ptp (props) {
     //     navigate('/settingspay', {replace: true})
     // }
 
-    const handleClickMyOrders = () => {
-        navigate('/myorders', {replace: true})
-    }
+    // const handleClickMyOrders = () => {
+    //     navigate('/myorders', {replace: true})
+    // }
 
     const backScreen = () => {
         navigate('/', {replace: true})
@@ -54,21 +54,24 @@ export function Ptp (props) {
 
 	const renderlistLastDeals = user_deals.slice(0, 3).map((deal, index) => {
 		return (
-				<div key={index} className='container-deal row' onClick={() => {handleClickDeal(deal)}}>
+				<div key={index} className='container-deal' onClick={() => {handleClickDeal(deal)}}>
 					<div className='deal-col-1'>
-						<div className='text-deal'><span className='label-deal'>From:</span> {deal.user_from}</div>
-						<div className='text-deal'><span className='label-deal'>To:</span> {deal.user_to}</div>
+						{/* <div className='text-deal'><span className='label-deal'>From:</span> {deal.user_from}</div>
+						<div className='text-deal'><span className='label-deal'>To:</span> {deal.user_to}</div> */}
+                        <div className='text-deal-date'> {deal.datetime.slice(0, 10)}</div>
+						<div className='text-deal-request-from'>{`Запрос от ${deal.user_to}`}</div>
 					</div>
 					<div className='deal-col-2'>
-						<div className='text-deal'><span className='label-deal'>Кол-во:</span> {deal.quantity} USDT</div>
-						<div className='text-deal text-nowrap'><span className='label-deal'>Статус: </span> 
+						<div className='text-deal-quantity'>{deal.quantity} USDT</div>
+                        <div className='text-deal-q-fiat'>${deal.quantity}</div>
+						{/* <div className='text-deal text-nowrap'><span className='label-deal'>Статус: </span> 
 							{
 								deal.status === 'request' ? 'Запрос':
 								deal.status === 'pay' ? 'Ожидание оплаты':
                                 deal.status === 'cancel' ? 'Отменена':
 								'Завершена'
 							}
-						</div>
+						</div> */}
 					</div>
 				</div>
 		)
@@ -89,56 +92,91 @@ export function Ptp (props) {
     //     setContent('complete_deal')
     // }
 
-    const arrow_right = 
-        <div className='trade-menu-arrow-col'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-            </svg>
-        </div>
+    // const arrow_right = 
+    //     <div className='trade-menu-arrow-col'>
+    //         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+    //             <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+    //         </svg>
+    //     </div>
 
 
     const create_order = 
-        <div className='row button-trade-menu' onClick={handleClickCreateOrder}>
-            <div className='trade-menu-plus-col'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                </svg>
-            </div>
-            <div className='trade-menu-text-col'>
+        <div className='row button-trade-menu'>
+            <div className='trade-menu-text-col' onClick={handleClickCreateOrder}>
                 Создать объявление
             </div>
-            {arrow_right}
+            <div className='trade-menu-text-col-2'>
+                
+            </div>
+        </div>
+
+    const faq = 
+        <div className='row button-trade-menu'>
+            <div className='trade-menu-text-col' onClick={() => {}}>
+                FAQ
+            </div>
+            <div className='trade-menu-text-col-2'>
+                
+            </div>
+        </div>
+
+    const support = 
+        <div className='row button-trade-menu'>
+            <div className='trade-menu-text-col' onClick={() => {}}>
+                Поддержка
+            </div>
+            <div className='trade-menu-text-col-2'>
+                
+            </div>
         </div>
 
 
     const menu_market = 
         <div className='row button-trade-menu'>
-            <div className='trade-menu-user-col'>
+            {/* <div className='trade-menu-user-col'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                 </svg>
-            </div>
+            </div> */}
             <div className='trade-menu-text-col' onClick={handleClickMarket}>
                 Маркет
             </div>
-            {arrow_right}
+            {/* {arrow_right} */}
+            <div className='trade-menu-text-col-2'>
+                Без комиссии
+            </div>
         </div>
 
-    const menu_my_orders = 
+    const person = 
         <div className='row button-trade-menu'>
-            <div className='trade-menu-user-col'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wallet-fill" viewBox="0 0 16 16">
-                    <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                    <path d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"/>
+            {/* <div className='trade-menu-user-col'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                 </svg>
+            </div> */}
+            <div className='trade-menu-text-col' onClick={handleClickMarket}>
+                Личный кабинет
             </div>
-            {/* <div className='trade-menu-text-col' onClick={handleClickSettingsPay}> */}
-            <div className='trade-menu-text-col' onClick={handleClickMyOrders}>
-                Мои объявления
+            {/* {arrow_right} */}
+            <div className='trade-menu-text-col-2'>
+                
             </div>
-            {arrow_right}
         </div>
+
+    // const menu_my_orders = 
+    //     <div className='row button-trade-menu'>
+    //         <div className='trade-menu-user-col'>
+    //             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wallet-fill" viewBox="0 0 16 16">
+    //                 <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
+    //                 <path d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"/>
+    //             </svg>
+    //         </div>
+    //         {/* <div className='trade-menu-text-col' onClick={handleClickSettingsPay}> */}
+    //         <div className='trade-menu-text-col' onClick={handleClickMyOrders}>
+    //             Мои объявления
+    //         </div>
+    //         {arrow_right}
+    //     </div>
 
     const divider = 
         <div className='divider'></div>
@@ -164,57 +202,34 @@ export function Ptp (props) {
         }, )
     return (
         <>
-            <div className='trade-menu-container p-2 m-3'>
-                {menu_market}
-                {divider}
-                {menu_my_orders}
-                {divider}
-                {create_order}
+        
+            <div className='d-flex justify-content-center'>
+                <div className='trade-menu-container'>
+                    {menu_market}
+                    <div className='d-flex justify-content-center'>{divider}</div>
+                    {person}
+                    <div className='d-flex justify-content-center'>{divider}</div>
+                    {create_order}
+                    <div className='d-flex justify-content-center'>{divider}</div>
+                    {faq}
+                    <div className='d-flex justify-content-center'>{divider}</div>
+                    {support}
+                </div>
+                
+                
             </div>
 
-            <div className='mt-5' style={{color: 'var(--btn-bg-color)'}}>Последние сделки</div>
-			
-			{renderlistLastDeals}
-
-            
-
-            {/* {
-                content === 'orders' &&
-                <div>
-                    <div className='my-order-container mt-5'>
-                        <div className='title-myorders'  >Мои объявления</div>
-                        {my_orders.map((order) => {
-                                return (
-                                    <OrderItem onClick={handleClickOrder} order={order} key={order.id}/>
-                                )
-                            })}
-                    </div>
+            <div className='d-flex justify-content-center'>
+                <div className='container-menu'>
+                    < div className='title-last-deals'>Последние сделки</div>
                 </div>
-            } */}
+            </div>
 
-            {/* {   content === 'deals' &&
-                <div className='mt-5'>
-                    <label style={{color: 'var(--text-light-color)'}}>Запросы</label>
-                    {order_deals.map((deal, index) => {
-                                return (
-                                    <div key={index} className='buyer-item my-2' onClick={() => {handleClickDeal(deal)}}>
-                                        <div style={{textAlign: 'left', color: 'var(--btn-bg-color)'}}>{deal.first_name}</div>
-                                        <div className='row d-flex justify-content-between'>
-                                            <div style={{width: '40vw', textAlign: 'left'}}>Сумма</div>
-                                            <div style={{width: '40vw'}}>{deal.quantity} USDT</div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
+            <div className='d-flex justify-content-center'>
+                <div className='container-menu'>
+                    {renderlistLastDeals}
                 </div>
-            }
-
-            {   content === 'complete_deal' &&
-                <div>
-                    <CompleteDeal handleClose={() => setContent('orders')}/>
-                </div>
-            } */}
-            
+            </div>
         </>
       );
 }
