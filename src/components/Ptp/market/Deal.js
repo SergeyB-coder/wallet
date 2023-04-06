fdabee848c8d104d80a804d4d8a3ce4773d229f6
Corpulent_Lymph_cc6a <senik1806@gmail.com>
2023-04-06 13:48:04 +0300

    50

--- src/components/Ptp/market/Deal.js
+++ src/components/Ptp/market/Deal.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useTelegram } from '../../../hooks/useTelegram';
-import { ButtonNext } from '../../Common/buttonNext';
+// import { ButtonNext } from '../../Common/buttonNext';
 import { getDealInfo, sendConfirm, sendEndDeal } from './marketApi';
 import { selectDealScreenInfo, setDealInfo, setDealScreenInfo } from './marketSlice';
+import { svg_clock, svg_hands, svg_salute } from '../../../const/svgs';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { useTelegram } from '../../../hooks/useTelegram';
 // import { ButtonNext } from '../buttonNext';
@@ -24,7 +25,7 @@ export function Deal () {
     const [showLoader, setShowLoader] = useState(false)
     const [error, setError] = useState('')
 
-    const str_type_deal = deal_screen_info?.type_order === 'b' ? 'Вы продаете': 'Вы покупаете у'
+    const str_type_deal = deal_screen_info?.type_order === 'b' ? 'Вы продаете': 'Вы покупаете'
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     function handleGetDealInfo() {
@@ -45,9 +46,9 @@ export function Deal () {
         }) 
     }
 
-    const handleClickRefresh = () => {
-         handleGetDealInfo(deal_screen_info.deal_id)
-    }
+    // const handleClickRefresh = () => {
+    //      handleGetDealInfo(deal_screen_info.deal_id)
+    // }
 
     const hanldeConfirm = () => {
         sendConfirm({deal_id: deal_screen_info.deal_id, saler_id: deal_screen_info.saler_id}, (data) => {
@@ -99,94 +100,181 @@ export function Deal () {
         }, )
 
     return (
-        <>
-            <div className='title-buy-label mt-3'>Сделка № {deal_screen_info?.deal_id}</div>
-            <div className='title-buy'>{str_type_deal} {deal_screen_info?.saler}</div>
-            
+        <div className='container-center'>
+            <div style={{width: '335px'}}>
+                <div className='title-buy-label mt-20'>Сделка № {deal_screen_info?.deal_id}</div>
+                {/* <div className='title-buy'>{str_type_deal} {deal_screen_info?.saler}</div> */}
+                <div className='title-buy mt-20'>{str_type_deal}</div>
+                
 
-            {/* <label style={{color: 'var(--text-light-color)'}}>Количество:</label> */}
-            <div style={{color: 'var(--text-light-color)', fontSize: 20}}>{deal_screen_info?.quantity} USDT {deal_screen_info?.currency === 1 ? 'BEP20': 'TRC20'}</div>
+                {/* <label style={{color: 'var(--text-light-color)'}}>Количество:</label> */}
+                <div className='title-buy-2'>{deal_screen_info?.quantity} USDT {deal_screen_info?.currency === 1 ? 'BEP20': 'TRC20'}</div>
 
-            <div className='screen-buy-container mt-5'>
-                <label style={{color: 'var(--btn-bg-color)', fontSize: 15}}>Статус</label><br></br>
-                {   deal_screen_info?.type_order === 's' ?
-                    (
-                        deal_screen_info?.status === "request" ? 'Ожидание подтверждения продавца': 
-                        showWait ? 'Дождитесь подтверждения об оплате': 'Продавец подтвердил зявку'
-                    ):
-                    (
-                        deal_screen_info?.status === "request" ? 'Ожидание подтверждения покупателя': 
-                        deal_screen_info?.status === 'pay' ? 'Ожидание оплаты':
-                        deal_screen_info?.status === 'confirm' ? 'Получатель подтвердил оплату':
-                        deal_screen_info?.status === 'end' ? 'Сделка совершена':
-                        ''
-                    )
+                <div className='saler-buyer mt-20'>{str_type_deal.charAt(3).toUpperCase() + str_type_deal.slice(4, str_type_deal.length-1)} {deal_screen_info?.saler}</div>
+
+                <div className='color-bg-cntr h-cntr-deal w-cntr mt-20'>
+                    {/* <label style={{color: 'var(--btn-bg-color)', fontSize: 15}}>Статус</label><br></br> */}
                     
-                }                
-            </div>
+                    <div className='container-center'>
+                        {showConfirmPay && deal_screen_info?.type_order === 's' ? svg_hands: 
+                        deal_screen_info?.status === 'end' ? svg_salute:
+                        svg_clock}
+                    </div>
 
-            <div className='deal-info-container mt-4 mb-3 mx-3'>
+                    <div className='wait-text'>
+                        {   deal_screen_info?.type_order === 's' ?
+                            (
+                                deal_screen_info?.status === "request" ? 'Ожиданием подтверждения': 
+                                showWait ? 'Дождитесь подтверждения об оплате': 
+                                deal_screen_info?.status === 'end' ? `Вам начислено ${deal_screen_info?.quantity} USDT`:
+                                'Продавец подтвердил зявку'
+                            ):
+                            (
+                                deal_screen_info?.status === "request" ? 'Ожидание подтверждения покупателя': 
+                                deal_screen_info?.status === 'pay' ? 'Ожидание оплаты':
+                                deal_screen_info?.status === 'confirm' ? 'Получатель подтвердил оплату':
+                                deal_screen_info?.status === 'end' ? 'Сделка совершена':
+                                ''
+                            )
+                        }   
+                    </div>      
+                    <div className='wait-text-1 mt-20'>
+                        {  
+                            deal_screen_info?.status === 'end' ? 'Сделка совершена': 
+                            
+                            deal_screen_info?.type_order === 's' ? 'от продавца': 
+                            
+                            'от покупателя'
+                        }  
+                    </div>       
+                </div>
 
-                <div className='row mb-3 mt-3'>
+                {/* <div className='deal-info-container mt-4 mb-3 mx-3'>
+
+                    <div className='row mb-3 mt-3'>
+                            <div className='buy-label'>
+                                Методы оплаты
+                            </div>
+                            <div className='buy-info'>
+                                {deal_screen_info?.company}
+                            </div>
+                    </div>
+
+                    <div className='row mt-3 mb-3'>
                         <div className='buy-label'>
-                            Методы оплаты
+                            Цена
                         </div>
-                        <div className='buy-info'>
-                            {deal_screen_info?.company}
+                        <div className='buy-info text-nowrap'>
+                            {deal_screen_info?.price} {deal_screen_info?.fiat === 1 ? 'RUB': 'USD'}
+                        </div>
+                    </div>
+
+                    <div className='row mt-3 mb-3'>
+                        <div className='buy-label'>
+                            Сумма покупки
+                        </div>
+                        <div className='buy-info text-nowrap'>
+                            {Math.round(deal_screen_info?.price * deal_screen_info?.quantity*1000)/1000} {deal_screen_info?.fiat === 1 ? 'RUB': 'USD'}
+                        </div>
+                    </div>
+
+                    <div onClick={handleClickRefresh}>
+                        {!showConfirmPay && 'Обновить'}
+                    </div>
+
+                </div> */}
+
+                <div className='container-center mt-20'>
+                        <div className='w-100'>
+                            <div className='order-row-1'>
+                                <div className='order-label-2'>
+                                    Методы оплаты
+                                </div>
+                                <div className='order-info-3'>
+                                    {deal_screen_info?.company}
+                                </div>
+                            </div>
+
+                            <div className='order-line-container'>
+                                <div className='order-line'></div>
+                            </div>
+
+                            <div className='order-row-1'>
+                                <div className='order-label-2'>
+                                    Цена
+                                </div>
+                                <div className='order-info-3'>
+                                    {deal_screen_info?.price} {deal_screen_info?.fiat === 1 ? 'RUB': 'USD'}
+                                </div>
+                            </div>
+
+                            <div className='order-line-container'>
+                                <div className='order-line'></div>
+                            </div>
+
+                            {/* <div className='row mt-3'>
+                                <div className='buy-label text-nowrap'>
+                                    Детали объявления
+                                </div> */}
+                                {/* <div className='buy-info'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                </div> */}
+                            {/* </div> */}
+
+                            <div className='order-row-1'>
+                                <div className='order-label-2'>
+                                    Сумма покупки
+                                </div>
+                                <div className='order-info-3'>
+                                    {Math.round(deal_screen_info?.price * deal_screen_info?.quantity*1000)/1000} {deal_screen_info?.fiat === 1 ? 'RUB': 'USD'}
+                                </div>
+                            </div>
                         </div>
                 </div>
 
-                <div className='row mt-3 mb-3'>
-                    <div className='buy-label'>
-                        Цена
-                    </div>
-                    <div className='buy-info text-nowrap'>
-                        {deal_screen_info?.price} {deal_screen_info?.fiat === 1 ? 'RUB': 'USD'}
-                    </div>
-                </div>
+                {showConfirmPay && deal_screen_info?.type_order === 's' &&
+                    // <div className='m-4'>
+                    //     <div className='label-deal-fiat'>Переведите фиаты по следующим реквизитам:</div>
+                    //     <div className='label-deal-fiat'>{deal_screen_info.company}</div>
+                    //     <div className='label-deal-fiat mb-3'>{deal_screen_info.card_number}</div>
+                        /* <ButtonNext text='Оплачено' onClick={hanldeConfirm}/> */
+                        <div onClick={hanldeConfirm} className='button-send-box button-active-send-bg active-text mt-20'>
+                            Подтвердить сделку
+                        </div>
+                    // </div>
+                }
 
-                <div className='row mt-3 mb-3'>
-                    <div className='buy-label'>
-                        Сумма покупки
+                {error !== 'Транзакция выполнена' &&
+                    showLoader ? 
+                    <div className="loader"></div>:
+                    <div className='m-3'>
+                        {deal_screen_info?.status === 'confirm' && deal_screen_info?.type_order === 'b' &&
+                            // <ButtonNext 
+                            //     text={'Подтвердить платеж'} 
+                            //     onClick={handleClickEndDeal}
+                            // />
+                            <div onClick={handleClickEndDeal} className='button-send-box button-active-send-bg active-text mt-20'>
+                                Подтвердить сделку
+                            </div>
+                        }
                     </div>
-                    <div className='buy-info text-nowrap'>
-                        {Math.round(deal_screen_info?.price * deal_screen_info?.quantity*1000)/1000} {deal_screen_info?.fiat === 1 ? 'RUB': 'USD'}
+                }
+
+                {/* <Chat deal_id={deal_screen_info.deal_id}/> */}
+                {/* <div onClick={() => {navigate('/chat/0', {replace: true})}} className='simple-btn'>
+                    Открыть чат
+                </div> */}
+
+                {deal_screen_info?.status === "request" &&
+                    <div className="button-send-box button-send-bg disable-text">
+                        63 секунды
                     </div>
-                </div>
-
-                <div onClick={handleClickRefresh}>
-                    {!showConfirmPay && 'Обновить'}
-                </div>
-
+                }
             </div>
-
-            {showConfirmPay && deal_screen_info?.type_order === 's' &&
-                <div className='m-4'>
-                    <div className='label-deal-fiat'>Переведите фиаты по следующим реквизитам:</div>
-                    <div className='label-deal-fiat'>{deal_screen_info.company}</div>
-                    <div className='label-deal-fiat mb-3'>{deal_screen_info.card_number}</div>
-                    <ButtonNext text='Оплачено' onClick={hanldeConfirm}/>
-                </div>
-            }
-
-            {error !== 'Транзакция выполнена' &&
-                showLoader ? 
-                <div className="loader"></div>:
-                <div className='m-3'>
-                    {deal_screen_info?.status === 'confirm' && deal_screen_info?.type_order === 'b' &&
-                        <ButtonNext 
-                            text={'Подтвердить платеж'} 
-                            onClick={handleClickEndDeal}
-                        />
-                    }
-                </div>
-            }
-
-            {/* <Chat deal_id={deal_screen_info.deal_id}/> */}
-            <div onClick={() => {navigate('/chat/0', {replace: true})}} className='simple-btn'>
-                Открыть чат
-            </div>
-        </>
+            
+        </div>
       );
 }
 
--- src/components/Ptp/market/screenBuy.js
+++ src/components/Ptp/market/screenBuy.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useTelegram } from '../../../hooks/useTelegram';
-import { ButtonNext } from '../../Common/buttonNext';
+// import { ButtonNext } from '../../Common/buttonNext';
 import { getOrderMethods, sendBuy } from './marketApi';
 import { selectQuantityBuy, setDealScreenInfo, setQuantityBuy } from './marketSlice';
 
@@ -20,8 +20,8 @@ export function ScreenBuy (props) {
     const is_buy = props.buyOrder.type === 'b'
 
     const handleChangeQuantity = (e) => {
-        // let inp = document.getElementById('quantity')
-        // inp.style.width = (20 + (e.target.value.length + 1) * 13) + 'px'
+        let inp = document.getElementById('q_buy')
+        inp.style.width = (11 + e.target.value.length * 11) + 'px'
         dispatch(setQuantityBuy(e.target.value))
     }
 
@@ -116,62 +116,112 @@ export function ScreenBuy (props) {
                     })}
                 </div>
             </div>:
-            <div className='screen-buy-container mt-5'>
-                <div className='title-buy'>{is_buy ? 'Продажа': 'Покупка у'} {props.buyOrder.first_name}</div>
+            <div className='container-center'>
+                <div className='screen-buy-container mt-20'>
 
-                <div className=' mt-5'>
-                    <label>Укажите количество:</label>
-                    {/* <div>
-                        <input id='quantity' className='bg-order-input' type='number' value={quantity_buy} onChange={handleChangeQuantity}/><span>USDT {props.buyOrder.currency_id === 1 ? 'BEP20': 'TRC20'}</span> 
-                    </div>   */}
-                    <div className='row address-item mt-2 mb-2 mx-2'>
-                        <div className='address-item-col1'>
-                            <div className=' p-0 m-0 d-flex align-items-center'>
-                                <input className='address-to-input' type='number' placeholder='0 USDT' onChange={handleChangeQuantity} value={quantity_buy}/>
+                    <div className='title-buy'>{is_buy ? 'Вы продаете ': 'Вы покупаете у '} {props.buyOrder.first_name}</div>
+
+                    <div className='container-buy-input mt-20'>
+                        <input id='q_buy' className='buy-input text-buy' type='number' placeholder='0' onChange={handleChangeQuantity} value={quantity_buy}/>
+                        <span className='text-buy '>USDT</span>
+                    </div>
+
+                    <div className='container-center mt-20'>
+                        <div className='price-info-buy'>Цена за 1 USDT {props.buyOrder.currency_id === 1 ? 'BEP20': 'TRC20'} = {props?.buyOrder?.price}</div>
+                    </div>
+                    
+                    <div className='container-center mt-20'>
+                        <div className='w-100'>
+                            <div className='order-row-1' onClick={handleClickMethodsPay}>
+                                <div className='order-label-2'>
+                                    Методы оплаты
+                                </div>
+                                <div className='order-info-3'>
+                                    {listMethodsPay[indexMethodPay]?.company_name || ''}
+                                </div>
                             </div>
-                        </div>
-                    </div>   
-                </div>
 
-                <div style={{color: 'var(--text-mini)', fontSize: 14, marginTop: 30}}>Цена за 1 USDT {props.buyOrder.currency_id === 1 ? 'BEP20': 'TRC20'} = {props?.buyOrder?.price}</div>
+                            <div className='order-line-container'>
+                                <div className='order-line'></div>
+                            </div>
 
-                <div className='methods-pay pt-3 pb-3 m-2 mt-5'>
-                    <div className='row mb-3' onClick={handleClickMethodsPay}>
-                        <div className='buy-label'>
-                            Методы оплаты
-                        </div>
-                        <div className='buy-info'>
-                            {listMethodsPay[indexMethodPay]?.company_name || ''}
+                            {/* <div className='row mb-3' onClick={handleClickMethodsPay}>
+                                <div className='buy-label'>
+                                    Методы оплаты
+                                </div>
+                                <div className='buy-info'>
+                                    {listMethodsPay[indexMethodPay]?.company_name || ''}
+                                </div>
+                            </div> */}
+
+                            {/* <div className='divider-order'></div> */}
+
+                            {/* <div className='row mt-3 mb-3'>
+                                <div className='buy-label'>
+                                    Лимит
+                                </div>
+                                <div className='buy-info text-nowrap'>
+                                    { props.buyOrder.limit_order} USDT
+                                </div>
+                            </div> */}
+
+                            <div className='order-row-1'>
+                                <div className='order-label-2'>
+                                    Лимиты
+                                </div>
+                                <div className='order-info-3'>
+                                    { props.buyOrder.limit_order} USDT
+                                </div>
+                            </div>
+
+                            <div className='order-line-container'>
+                                <div className='order-line'></div>
+                            </div>
+
+                            {/* <div className='row mt-3'>
+                                <div className='buy-label text-nowrap'>
+                                    Детали объявления
+                                </div> */}
+                                {/* <div className='buy-info'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                </div> */}
+                            {/* </div> */}
+
+                            <div className='order-row-1'>
+                                <div className='order-label-2'>
+                                    Детали объявления
+                                </div>
+                                <div className='order-info-3'>
+                                    <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M5 4C5 4.26522 4.89464 4.51957 4.70711 4.70711L1.70711 7.70711C1.31658 8.09763 0.683416 8.09763 0.292892 7.70711C-0.0976318 7.31658 -0.0976317 6.68342 0.292893 6.29289L2.58579 4L0.292893 1.70711C-0.097631 1.31658 -0.0976315 0.683417 0.292893 0.292893C0.683417 -0.0976313 1.31658 -0.0976312 1.70711 0.292893L4.70711 3.29289C4.89464 3.48043 5 3.73478 5 4Z" fill="white"/>
+                                    </svg>
+                                </div>
+                            </div>
+
+                            
+
                         </div>
                     </div>
-                    <div className='divider-order'></div>
-                    <div className='row mt-3 mb-3'>
-                        <div className='buy-label'>
-                            Лимит
-                        </div>
-                        <div className='buy-info text-nowrap'>
-                            { props.buyOrder.limit_order} USDT
-                        </div>
-                    </div>
-                    <div className='divider-order'></div>
-                    <div className='row mt-3'>
-                        <div className='buy-label text-nowrap'>
-                            Детали объявления
-                        </div>
-                        {/* <div className='buy-info'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>
-                        </div> */}
-                    </div>
-                    <div className='order-comment'>{props.buyOrder.comment}</div>
-                </div>
 
-                <div className='m-2 mt-5'>
-                    <ButtonNext text='Начать сделку' onClick={handleClickBuy}/>
-                </div>
+                    <div className='order-comment mt-20'>
+                        <div className='message-buy '>Внимательно прочтите следующие условия мерчанта перед размещением ордера. Несоблюдение условий может привести к неудачным транзакциям и финансовым потерям.</div>
+                        <div className='container-comment-text'>
+                            <div className='comment-text'>{props.buyOrder.comment}</div>
+                        </div>
+                    </div>
+
+                    {/* <div className='m-2 mt-5'>
+                        <ButtonNext text='Начать сделку' onClick={handleClickBuy}/>
+                    </div> */}
+                    <div onClick={handleClickBuy} className='button-send-box button-active-send-bg active-text mt-20'>
+                        Начать сделку
+                    </div>
                 
+                </div>
             </div>
+            
         }
         </>
       );
