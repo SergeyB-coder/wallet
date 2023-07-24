d6fc3d8fdceb8bef569aec659bb212a3478d2e55
Sergey Burdin <senik1806@gmail.com>
2023-07-24 08:49:18 +0300

    2007

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -4,13 +4,13 @@ export function useTelegram () {
     return {
         tg,
         query_id: tg.initDataUnsafe.query_id,
-        // user_id: tg.initDataUnsafe.user.id,
-        // first_name: tg.initDataUnsafe.user.first_name,
+        user_id: tg.initDataUnsafe.user.id,
+        first_name: tg.initDataUnsafe.user.first_name,
         // user_id: 638484379,
         // first_name: 'Sash'
         // user_id: '2125428302',
-        user_id: 652065848,
-        first_name: 'Srg',
+        // user_id: 652065848,
+        // first_name: 'Srg',
         // user_id: '1234567',
         // first_name: 'Super',
         // user_id: 671690853,
