f371e0a421b6d851a7d2c98493950a92d3916ec6
Sergey Burdin <senik1806@gmail.com>
2023-08-17 22:08:15 +0300

    0308

--- src/components/Common/funcs.js
+++ src/components/Common/funcs.js
@@ -1,5 +1,5 @@
 export function dateConvert (s) {
     const month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
-    const date = s.slice(8, 10) + ' ' + month[parseInt(s.slice(5, 7))] + ' ' + s.slice(0, 4) + ', ' + s.slice(11, 13) + ':' + s.slice(14, 16)
+    const date = s.slice(8, 10) + ' ' + month[parseInt(s.slice(5, 7))-1] + ' ' + s.slice(0, 4) + ', ' + s.slice(11, 13) + ':' + s.slice(14, 16)
     return date
 }
