31b45189648d4d4a0b092b060a39dbb5cb616952
Sergey Burdin <senik1806@gmail.com>
2023-08-21 11:30:56 +0300

    0308

--- src/components/Ptp/ptp.js
+++ src/components/Ptp/ptp.js
@@ -89,7 +89,7 @@ export function Ptp(props) {
             //     </div>
             // </div>
 
-            <div key={index} className={`row-2 a-c ${true ? 'color-bg-deal' : 'color-bg-deal-r'} h-77 p-17 mt-20`}>
+            <div key={index} className={`row-2 a-c ${true ? 'color-bg-deal' : 'color-bg-deal-r'} h-77 p-17 mt-20`} onClick={() => { handleClickDeal(deal) }}>
                 <div className='h-100'>
                     <div className='deal-text mt-18'>{deal?.datetime?.slice(0, 10)}</div>
                     <div className={true ? 'deal-text-2 color-deal-text mt-10' : 'mt-10 deal-text-2 color-deal-r-text'}>
